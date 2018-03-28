f7e89ebef00884ffc3288576a7a71dd45ba1e61a
Ryan Herbst <rherbst@slac.stanford.edu>
2018-03-28 09:49:22 -0700

    Removed axi generic

--- targets/DtmTest/hdl/DtmRtmTest.vhd
+++ targets/DtmTest/hdl/DtmRtmTest.vhd
@@ -565,7 +565,6 @@ begin
       generic map (
          TPD_G                      => 1 ns,
          STATUS_CNT_WIDTH_G         => 32,
-         AXI_ERROR_RESP_G           => AXI_RESP_OK_C,
          ALTERA_SYN_G               => false,
          ALTERA_RAM_G               => "M9K",
          CASCADE_SIZE_G             => 1,
