3511dcbc752526ead4721745acc81bf0ec2d7710
Larry Ruckman <ruckman@slac.stanford.edu>
2018-08-31 09:28:44 -0700

    major reorg of targets

---
+++ targets/empty/DpmEmpty/hdl/DpmEmpty.vhd
@@ -0,0 +1,165 @@
+-------------------------------------------------------------------------------
+-- File       : DpmEmpty.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2018-06-13
+-- Last update: 2018-08-30
+-------------------------------------------------------------------------------
+-- Description: Top Level Firmware Target
+-------------------------------------------------------------------------------
+-- This file is part of 'RCE Development Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'RCE Development Firmware', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiPkg.all;
+use work.AxiStreamPkg.all;
+use work.RceG3Pkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity DpmEmpty is
+   generic (
+      TPD_G        : time := 1 ns;
+      BUILD_INFO_G : BuildInfoType);
+   port (
+      -- Debug
+      led         : out   slv(1 downto 0);
+      -- I2C
+      i2cSda      : inout sl;
+      i2cScl      : inout sl;
+      -- Ethernet
+      ethRxP      : in    slv(3 downto 0);
+      ethRxM      : in    slv(3 downto 0);
+      ethTxP      : out   slv(3 downto 0);
+      ethTxM      : out   slv(3 downto 0);
+      ethRefClkP  : in    sl;
+      ethRefClkM  : in    sl;
+      -- RTM High Speed
+      dpmToRtmHsP : out   slv(11 downto 0);
+      dpmToRtmHsM : out   slv(11 downto 0);
+      rtmToDpmHsP : in    slv(11 downto 0);
+      rtmToDpmHsM : in    slv(11 downto 0);
+      -- Reference Clocks
+      locRefClkP  : in    sl;
+      locRefClkM  : in    sl;
+      dtmRefClkP  : in    sl;
+      dtmRefClkM  : in    sl;
+      -- DTM Signals
+      dtmClkP     : in    slv(1 downto 0);
+      dtmClkM     : in    slv(1 downto 0);
+      dtmFbP      : out   sl;
+      dtmFbM      : out   sl;
+      -- Clock Select
+      clkSelA     : out   slv(1 downto 0);
+      clkSelB     : out   slv(1 downto 0));
+end DpmEmpty;
+
+architecture TOP_LEVEL of DpmEmpty is
+
+   signal axilClk         : sl;
+   signal axilRst         : sl;
+   signal axilReadMaster  : AxiLiteReadMasterType  := AXI_LITE_READ_MASTER_INIT_C;
+   signal axilReadSlave   : AxiLiteReadSlaveType   := AXI_LITE_READ_SLAVE_EMPTY_OK_C;
+   signal axilWriteMaster : AxiLiteWriteMasterType := AXI_LITE_WRITE_MASTER_INIT_C;
+   signal axilWriteSlave  : AxiLiteWriteSlaveType  := AXI_LITE_WRITE_SLAVE_EMPTY_OK_C;
+
+   signal dmaClk      : slv(2 downto 0);
+   signal dmaRst      : slv(2 downto 0);
+   signal dmaIbMaster : AxiStreamMasterArray(2 downto 0);
+   signal dmaIbSlave  : AxiStreamSlaveArray(2 downto 0);
+   signal dmaObMaster : AxiStreamMasterArray(2 downto 0);
+   signal dmaObSlave  : AxiStreamSlaveArray(2 downto 0);
+
+begin
+
+   led <= "00";
+
+   --------------------------------------------------
+   -- Core
+   --------------------------------------------------
+   U_DpmCore : entity work.DpmCore
+      generic map (
+         TPD_G          => TPD_G,
+         BUILD_INFO_G   => BUILD_INFO_G,
+         RCE_DMA_MODE_G => RCE_DMA_AXISV2_C,
+         ETH_TYPE_G     => "10GBASE-KR")
+      port map (
+         -- IPMI I2C Ports
+         i2cSda             => i2cSda,
+         i2cScl             => i2cScl,
+         -- Clock Select
+         clkSelA            => clkSelA,
+         clkSelB            => clkSelB,
+         -- Ethernet Ports
+         ethRxP             => ethRxP,
+         ethRxM             => ethRxM,
+         ethTxP             => ethTxP,
+         ethTxM             => ethTxM,
+         ethRefClkP         => ethRefClkP,
+         ethRefClkM         => ethRefClkM,
+         -- AXI-Lite Register Interface [0xA0000000:0xAFFFFFFF]
+         axiClk             => axilClk,
+         axiClkRst          => axilRst,
+         extAxilReadMaster  => axilReadMaster,
+         extAxilReadSlave   => axilReadSlave,
+         extAxilWriteMaster => axilWriteMaster,
+         extAxilWriteSlave  => axilWriteSlave,
+         -- AXI Stream DMA Interfaces
+         dmaClk             => dmaClk,
+         dmaClkRst          => dmaRst,
+         dmaObMaster        => dmaObMaster,
+         dmaObSlave         => dmaObSlave,
+         dmaIbMaster        => dmaIbMaster,
+         dmaIbSlave         => dmaIbSlave);
+
+   dmaClk      <= (others => axilClk);
+   dmaRst      <= (others => axilRst);
+   dmaIbMaster <= dmaObMaster;
+   dmaObSlave  <= dmaIbSlave;
+
+   ----------
+   -- RTM GTs
+   ----------
+   U_TERM_GTs : entity work.Gtxe2ChannelDummy
+      generic map (
+         TPD_G   => TPD_G,
+         WIDTH_G => 12)
+      port map (
+         refClk => axilClk,
+         gtRxP  => rtmToDpmHsP,
+         gtRxN  => rtmToDpmHsM,
+         gtTxP  => dpmToRtmHsP,
+         gtTxN  => dpmToRtmHsM);
+
+   ------------
+   -- DTM Clock
+   ------------
+   U_DtmClkgen : for i in 1 downto 0 generate
+      U_DtmClkIn : IBUFDS
+         generic map (DIFF_TERM => true)
+         port map(
+            I  => dtmClkP(i),
+            IB => dtmClkM(i),
+            O  => open);
+   end generate;
+
+   ---------------
+   -- DTM Feedback
+   ---------------
+   U_DtmFbOut : OBUFDS
+      port map(
+         O  => dtmFbP,
+         OB => dtmFbM,
+         I  => '0');
+
+end TOP_LEVEL;
---
+++ targets/empty/HsioDtmEmpty/hdl/HsioDtmEmpty.vhd
@@ -0,0 +1,139 @@
+-------------------------------------------------------------------------------
+-- File       : HsioDtmEmpty.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2018-05-01
+-- Last update: 2018-08-30
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of 'RCE Development Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'RCE Development Firmware', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiPkg.all;
+use work.AxiStreamPkg.all;
+use work.RceG3Pkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity HsioDtmEmpty is
+   generic (
+      TPD_G        : time := 1 ns;
+      BUILD_INFO_G : BuildInfoType);
+   port (
+      -- Debug
+      led        : out   slv(1 downto 0);
+      -- I2C
+      i2cSda     : inout sl;
+      i2cScl     : inout sl;
+      -- Clock Select
+      clkSelA    : out   sl;
+      clkSelB    : out   sl;
+      -- Base Ethernet
+      ethRxCtrl  : in    slv(1 downto 0);
+      ethRxClk   : in    slv(1 downto 0);
+      ethRxDataA : in    Slv(1 downto 0);
+      ethRxDataB : in    Slv(1 downto 0);
+      ethRxDataC : in    Slv(1 downto 0);
+      ethRxDataD : in    Slv(1 downto 0);
+      ethTxCtrl  : out   slv(1 downto 0);
+      ethTxClk   : out   slv(1 downto 0);
+      ethTxDataA : out   Slv(1 downto 0);
+      ethTxDataB : out   Slv(1 downto 0);
+      ethTxDataC : out   Slv(1 downto 0);
+      ethTxDataD : out   Slv(1 downto 0);
+      ethMdc     : out   Slv(1 downto 0);
+      ethMio     : inout Slv(1 downto 0);
+      ethResetL  : out   Slv(1 downto 0);
+      -- IPMI
+      dtmToIpmiP : out   slv(1 downto 0);
+      dtmToIpmiM : out   slv(1 downto 0));
+end HsioDtmEmpty;
+
+architecture TOP_LEVEL of HsioDtmEmpty is
+
+   signal axilClk         : sl;
+   signal axilRst         : sl;
+   signal axilReadMaster  : AxiLiteReadMasterType  := AXI_LITE_READ_MASTER_INIT_C;
+   signal axilReadSlave   : AxiLiteReadSlaveType   := AXI_LITE_READ_SLAVE_EMPTY_OK_C;
+   signal axilWriteMaster : AxiLiteWriteMasterType := AXI_LITE_WRITE_MASTER_INIT_C;
+   signal axilWriteSlave  : AxiLiteWriteSlaveType  := AXI_LITE_WRITE_SLAVE_EMPTY_OK_C;
+
+   signal dmaClk      : slv(3 downto 0);
+   signal dmaRst      : slv(3 downto 0);
+   signal dmaIbMaster : AxiStreamMasterArray(3 downto 0);
+   signal dmaIbSlave  : AxiStreamSlaveArray(3 downto 0);
+   signal dmaObMaster : AxiStreamMasterArray(3 downto 0);
+   signal dmaObSlave  : AxiStreamSlaveArray(3 downto 0);
+
+begin
+
+   led <= "00";
+
+   -----------
+   -- DTM Core
+   -----------
+   U_HsioCore : entity work.HsioCore
+      generic map (
+         TPD_G          => TPD_G,
+         BUILD_INFO_G   => BUILD_INFO_G,
+         RCE_DMA_MODE_G => RCE_DMA_AXISV2_C)  -- AXIS V2 Driver
+      port map (
+         -- I2C
+         i2cSda             => i2cSda,
+         i2cScl             => i2cScl,
+         -- Clock Select
+         clkSelA            => clkSelA,
+         clkSelB            => clkSelB,
+         -- Base Ethernet
+         ethRxCtrl          => ethRxCtrl,
+         ethRxClk           => ethRxClk,
+         ethRxDataA         => ethRxDataA,
+         ethRxDataB         => ethRxDataB,
+         ethRxDataC         => ethRxDataC,
+         ethRxDataD         => ethRxDataD,
+         ethTxCtrl          => ethTxCtrl,
+         ethTxClk           => ethTxClk,
+         ethTxDataA         => ethTxDataA,
+         ethTxDataB         => ethTxDataB,
+         ethTxDataC         => ethTxDataC,
+         ethTxDataD         => ethTxDataD,
+         ethMdc             => ethMdc,
+         ethMio             => ethMio,
+         ethResetL          => ethResetL,
+         -- IPMI
+         dtmToIpmiP         => dtmToIpmiP,
+         dtmToIpmiM         => dtmToIpmiM,
+         -- External Axi Bus, 0xA0000000 - 0xAFFFFFFF
+         axiClk             => axilClk,
+         axiClkRst          => axilRst,
+         extAxilReadMaster  => axilReadMaster,
+         extAxilReadSlave   => axilReadSlave,
+         extAxilWriteMaster => axilWriteMaster,
+         extAxilWriteSlave  => axilWriteSlave,
+         -- DMA Interfaces
+         dmaClk             => dmaClk,
+         dmaClkRst          => dmaRst,
+         dmaObMaster        => dmaObMaster,
+         dmaObSlave         => dmaObSlave,
+         dmaIbMaster        => dmaIbMaster,
+         dmaIbSlave         => dmaIbSlave);
+
+
+   dmaClk      <= (others => axilClk);
+   dmaRst      <= (others => axilRst);
+   dmaIbMaster <= dmaObMaster;
+   dmaObSlave  <= dmaIbSlave;
+
+end TOP_LEVEL;
